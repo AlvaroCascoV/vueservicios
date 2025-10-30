382b8de661e38f9e52ea99a9dbb37bf2c858fc15
AlvaroCascoV <alvarocascovalero@gmail.com>
2025-10-30 11:53:20 +0100

    axios get

---
+++ src/components/CochesComponent.vue
@@ -0,0 +1,35 @@
+<template>
+	<h1>Api Coches</h1>
+	<div v-for="car in coches" :key="car">
+		<h3 style="color: blue">{{ car.marca }} {{ car.modelo }}</h3>
+		<p>Driver; {{ car.conductor }}</p>
+		<img :src="car.imagen" style="width: 150px; height: 150px" />
+	</div>
+</template>
+
+<script>
+	import axios from "axios";
+	//SI NECESITAMOS VARIABLES PARA TODO EL COMPONENTE Y SUS METODOS
+	//SE DECLARAN AQUI
+	let urlApi = "https://apicochespaco.azurewebsites.net/";
+	export default {
+		name: "CochesComponent",
+		data() {
+			return {
+				coches: [],
+			};
+		},
+		methods: {
+			getCoches() {
+				let request = "webresources/coches";
+				//LAS VARIABLES DECLARADAS FUERA DEL EXPORT NO UTILIZAN "this"
+				axios.get(urlApi + request).then((response) => {
+					this.coches = response.data;
+				});
+			},
+		},
+		mounted() {
+			this.getCoches();
+		},
+	};
+</script>
